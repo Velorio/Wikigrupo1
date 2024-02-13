db05e87890540b2aeb96dc69d2290fada2a4c807
Saul Arias <s.a.u.1.ariasss@Sauls-MacBook-Pro.local>
2024-02-13 18:59:11 -0500

    se crea el proyecto

---
+++ src/test/java/com/example/demo/Entity/Form.java
@@ -0,0 +1,30 @@
+package com.example.demo.Entity;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Form {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String url;
+    private String code;
+    private String internalCode;
+
+}
